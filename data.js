74c0be8b7d5b5c5efb8dd39c09ac079a974ed83d
Lillian Chernin <lillian.chernin@gmail.com>
2023-02-15 15:45:20 -0600

    added header and first js files

---
+++ data.js
@@ -0,0 +1,54 @@
+const reviews = [
+    {
+        "review": "Dominic did an excellent job tutoring my son in precalculus. He was knowledgeable, reliable and personable. I really appreciated how accommodating he was whenever my son requested additional tutoring before a test. Everything about the experience was positive.",
+        "reviwerName": "Michele J."
+    },
+    {
+        "review": "Dominic has had such an amazing impact on my life over the last year. The first time we met, it was the day his daughter was born! That's true dedication to his students, and he hadn't even met me yet. Since then, not only have I LEARNED math, but I've started to understand WHY math works the way it does. We can converse about anything - math or non math aligned - and he knows all about it. There is never a question that goes unanswered when he is around. Dominic loves math, and his passion has spread into mine, taking me from someone who thought of a number as just another button on a calculator to someone who sees the world mathematically. To top all of this off, he has really cute cats :)",
+        "reviwerName": "Arjun B."
+    },
+    {
+        "review": "Dominic has provided weekly online tutoring for our sons in algebra, geometry and calculus.  He is an energetic and engaging tutor. Our oldest scored a 5 on his AP Calculus exam and credits working with Dominic as the reason “Hands down the best math teacher I’ve ever had. He just loves math.”  Dominic is just as effective teaching lower level math classes.  We have had such a positive experience with Dominic that we were delighted to discover that Katherine was available for chemistry tutoring. Katherine is knowledgeable and patient. As a parent, I have also appreciated her insights, partnership and adjustments to make sure our son is making progress. Dominic and Katherine are incredibly responsive on scheduling — just an overall positive experience!",
+        "reviwerName": "Kimberly E."
+    },
+    {
+        "review": "My son was taking honors math, and after a rather poor start of the semester, he needed help. Dominic thoroughly reviewed the material with him, and explained the topics that my son had struggled with. Dominic provided different methods to solve problems, and tips on how to remember them. All this really helped my son, and he finished the semester much better than he started. In addition, Dominic is responsive and he responds promptly to my e-mails.",
+        "reviwerName": "Wladek"
+    },
+    {
+        "review": "Dominic was an outstanding math tutor that guided me through Calculus AB. With prior experience in math, Dominic showed me different learning methods that helped my math skills along with various examples. Additionally, Dominic provided excellent AP test material that helped me earn college credit. My favorite part about Dominic is that he not only helps you with math but also values the relationship between tutor and student.",
+        "reviwerName": "Kenny, Materials Engineering student at Cal Poly SLO"
+    },
+    {
+        "review": "I would highly recommend Dominic Lipari for any tutoring services regarding math. He is approachable, patient, and honest that at times even he doesn't know everything. He's willing to at times be flexible and is understanding about if you make mistakes or have trouble processing things. I found him to be a very good tutor and I wouldn't have been able to do well in my classes without him.",
+        "reviwerName": "Asher"
+    }
+]
+
+const faqs = [
+    {
+        "question": "For what classes can you provide tutoring?",
+        "answer": "Dominic’s expertise is in all levels of math, as well as Physics. He can tutor students from elementary school through AP® Calculus AB (with the exception of Statistics). Though he is willing to work with anyone, his experience has been mostly with middle and high school students. Note: Dominic has not tutored AP® Calculus BC or AP® Physics for a few years, and might be a bit rusty. He is always willing to try and be of help, but cannot guarantee his expertise in all situations. Katherine’s expertise is in all levels of Biology and Chemistry, through college level classes."
+    },
+    {
+        "question": "Can I reschedule my appointment or add additional appointments if there’s an upcoming quiz or test?",
+        "answer": "Of course! We always try to make adjustments to our schedule in order to accommodate the most immediate needs of our students. Though we cannot always guarantee there will be available spots, we will always see if there’s a way."
+    },
+    {
+        "question": "Can I just schedule appointments whenever I need help, rather than set up recurring appointments?",
+        "answer": "Yes. The benefits of recurring appointments are substantial (improved working relationship between tutor and student, a greater ability to tailor our tutoring to the particular student), so we always encourage regular weekly meetings. However, we understand that not everyone is in the position to meet weekly and we are happy to fit students in as needed if we are available."
+    },
+    {
+        "question": "Do you do in-person tutoring?",
+        "answer": "Though we do not typically tutor in person, we will occasionally meet with students either at a library or in our home. Contact us for availability."
+    },
+    {
+        "question": "Do you offer discounts for multiple students?",
+        "answer": "We offer discounted rates if you set up regular appointments for more than one student in different subjects. We can also negotiate package rates for working with multiple students at a time. Contact us for more information."
+    }
+    // ,
+    // {
+    //     "question": "What is your cancellation policy?",
+    //     "answer": ""
+    // }
+]
