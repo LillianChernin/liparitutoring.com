73d566954761afa1f3d77d6e4ee5ee5358df41c5
Lillian Chernin <lillian.chernin@gmail.com>
2023-03-16 18:35:58 -0500

    updated FAQs, added pictures to homepage and added links to google form for contact

--- faqs.js
+++ faqs.js
@@ -1,37 +1,42 @@
-const questionSymbolText = "Q: ";
-const answerSymbolText = "A: ";
+// const googleForm = document.getElementById("googleForm");
+// document.onresize = function() {
+//     googleForm.setAttribute("width")
+// }
 
-const faqsContainer = document.getElementsByClassName('faqsContainer')[0];
-faqs.forEach(populateFAQs);
+// const questionSymbolText = "Q: ";
+// const answerSymbolText = "A: ";
 
-function populateFAQs(value, index, array) {
-    let faq = document.createElement('div');
-    let questionSymbol = document.createElement('div');
-    let question = document.createElement('div');
-    let spacer = document.createElement('div');
-    let answerSymbol = document.createElement('div');
-    let answer = document.createElement('div');
+// const faqsContainer = document.getElementsByClassName('faqsContainer')[0];
+// faqs.forEach(populateFAQs);
+
+// function populateFAQs(value, index, array) {
+//     let faq = document.createElement('div');
+//     let questionSymbol = document.createElement('div');
+//     let question = document.createElement('div');
+//     let spacer = document.createElement('div');
+//     let answerSymbol = document.createElement('div');
+//     let answer = document.createElement('div');
     
     
-    faq.classList.add("faq");
-    questionSymbol.classList.add("questionSymbol");
-    question.classList.add("question");
-    spacer.classList.add("spacer");
-    answerSymbol.classList.add("answerSymbol");
-    answer.classList.add("answer");
+//     faq.classList.add("faq");
+//     questionSymbol.classList.add("questionSymbol");
+//     question.classList.add("question");
+//     spacer.classList.add("spacer");
+//     answerSymbol.classList.add("answerSymbol");
+//     answer.classList.add("answer");
     
     
-    questionSymbol.appendChild(document.createTextNode(questionSymbolText));
-    question.appendChild(document.createTextNode(value["question"]));
-    answerSymbol.appendChild(document.createTextNode(answerSymbolText));
-    answer.appendChild(document.createTextNode(value["answer"]));
+//     questionSymbol.appendChild(document.createTextNode(questionSymbolText));
+//     question.appendChild(document.createTextNode(value["question"]));
+//     answerSymbol.appendChild(document.createTextNode(answerSymbolText));
+//     answer.appendChild(document.createTextNode(value["answer"]));
     
-    faq.appendChild(questionSymbol);
-    faq.appendChild(question);
-    faq.appendChild(spacer);
-    faq.appendChild(answerSymbol);
-    faq.appendChild(answer);
+//     faq.appendChild(questionSymbol);
+//     faq.appendChild(question);
+//     faq.appendChild(spacer);
+//     faq.appendChild(answerSymbol);
+//     faq.appendChild(answer);
     
 
-    faqsContainer.appendChild(faq);
-}
+//     faqsContainer.appendChild(faq);
+// }
